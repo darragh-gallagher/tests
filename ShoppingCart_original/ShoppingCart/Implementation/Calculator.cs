7c08911afa0468dc376d034bffa65e519c8ec2d1
Darragh <79007696+darragh-gallagher@users.noreply.github.com>
2022-03-31 17:23:13 +0100

    original sc

---
+++ ShoppingCart_original/ShoppingCart/Implementation/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ShoppingCart.Interfaces;
+using ShoppingCart.Pocos;
+using System;
+
+namespace ShoppingCart.Implementation
+{
+    public class Calculator
+    {
+        private readonly IRepository<Product> _productStore;
+        private readonly IShippingCalculator _shippingCalculator;
+
+        public Calculator(IShippingCalculator shippingCalculator, IRepository<Product> productStore)
+        {
+            _productStore = productStore;
+            _shippingCalculator = shippingCalculator;
+        }
+
+        public double Total(IList<CartItem> cart)
+        {
+            throw new NotImplementedException("Cart total calculation not implemented");
+        }
+    }
+}
---
+++ ShoppingCart_original/ShoppingCart/Implementation/ShippingCalculator.cs
@@ -0,0 +1,13 @@
+﻿using ShoppingCart.Interfaces;
+using System;
+
+namespace ShoppingCart.Implementation
+{
+    public class ShippingCalculator : IShippingCalculator
+    {
+        public double CalcShipping(double cartTotal)
+        {
+            throw new NotImplementedException("Shipping calculation not implemented");
+        }
+    }
+}
